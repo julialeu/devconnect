b4909f361a3d910c360e5804fd3a37cfde77c34c
Julia Leuenberger <leuenberger2101@gmail.com>
2023-06-17 18:50:06 +0200

    Delete likes

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -32,4 +32,9 @@ class Post extends Model
         return $this->hasMany(Like::class);
     }
 
+    public function checkLike(User $user)
+    {
+        return $this->likes->contains('user_id', $user->id);
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -41,6 +41,7 @@ Route::post('/{user:username}/posts/{post}', [CommentController::class, 'store']
 Route::post('/imagenes', [ImageController::class, 'store'])->name('imagenes.store');
 
 Route::post('/posts/{post}/likes', [LikeController::class, 'store'])->name('posts.likes.store');
+Route::delete('/posts/{post}/likes', [LikeController::class, 'destroy'])->name('posts.likes.destroy');
 
 
 
