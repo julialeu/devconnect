569fd6bc20cd2c0cdfa238dd1d0bb7418d43aea8
Julia Leuenberger <leuenberger2101@gmail.com>
2023-06-21 11:34:33 +0200

    Create form to edit profile

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -5,5 +5,37 @@
 @endsection
 
 @section('contenido')
-
+    <div class="md:flex md:justify-center">
+        <div class="md:w-1/2 bg-white shadow p-6">
+            <form class="mt-10 md:mt-0">
+                <div class="mb-5">
+                    <label for="username" class="mb-2 block uppercase text-gray-500 font-bold">Username</label>
+                    <input id="username"
+                           name="username"
+                           type="text"
+                           placeholder="Your Username"
+                           class="border p-3 w-full rounded-lg @error('username') border-red-500 @enderror"
+                           value="{{ auth()->user()->username }}" />
+                    @error('username')
+                    <p class="text-red-500 text-xs my-2 p-2">{{ $message }}</p>
+                    @enderror
+                </div>
+                <div class="mb-5">
+                    <label for="photo" class="mb-2 block uppercase text-gray-500 font-bold">Profile photo</label>
+                    <input id="photo"
+                           name="photo"
+                           type="file"
+                           class="border p-3 w-full rounded-lg"
+                           value=""
+                           accept=".jpg, .jpeg, .png"
+                    />
+                </div>
+                <input type="submit"
+                       value="Save changes"
+                       class="bg-sky-600 hover:bg-sky-700 transition-colors
+                       cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg"
+                />
+            </form>
+        </div>
+    </div>
 @endsection
